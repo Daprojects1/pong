90704fe0e454900dba9abe3c1db5e6ce228040c2
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-07-10 21:17:01 +0100

    need to allow for start on the other paddle

--- index.js
+++ index.js
@@ -37,43 +37,71 @@ document.addEventListener("DOMContentLoaded", () => {
         ctx.fill()
         ctx.closePath()
     }
+    const initialSettings = (mainObj) => {
+        const paddleSettings={
+            width: 10,
+            height: 50
+        }
+        return {
+            paddleSettings,
+            ballSpeed: 2.5,
+            round: 1,
+            scores: {
+                padd1:0,
+                padd2:0
+            },
+            maxSpeed: 3.5,
+            interval: null,
+            winner: null,
+            running: true,
+            paddle1: createPadd(0, mainObj.height / 2 - paddleSettings.height, 0, { left: 's', right: 'w' }),
+            paddle2:createPadd(mainObj.width - paddleSettings.width, mainObj.height / 2 - paddleSettings.height, 0,
+                { left: 'ArrowLeft', right: 'ArrowRight' }),
+        }
+    }
     class Board {
         constructor() {
             this.canvas = canvas
             this.ctx = canvas.getContext("2d")
             this.width = canvas.width
             this.height = canvas.height
-            this.maxSpeed = 15
-            this.round = 1
-            this.paddleSettings = {
-                width: 10,
-                height: 50
-            }
-            this.ballSpeed = 15
-            this.ballSettings = {
-                x: 20,
+            this.initialSettings = initialSettings(this)
+            this.ballStart = 'player1'
+            this.ballSettings ={
+                x: this.handleBallStartX(),
                 y :randomIntFromInterval(30,this.height-30),
                 radius:8,
-                xSpeed: 10,
-                ySpeed: 10,
+                xSpeed: 2.5,
+                ySpeed: 2.5,
             }
-            this.scores = {
-                padd1:0,
-                padd2:0
-            }
-            this.paddle1Settings = createPadd(0, this.height / 2 - this.paddleSettings.height, 0, { left: 's', right: 'w' })
-            this.paddle2Settings = createPadd(this.width - this.paddleSettings.width, this.height / 2 - this.paddleSettings.height, 0,
-                { left: 'ArrowLeft', right: 'ArrowRight' })
+            // Initial settings obj makes it easier to reset after every round
+            this.maxSpeed = this.initialSettings.maxSpeed
+            this.round = this.initialSettings.round
+            this.paddleSettings = {...this.initialSettings.paddleSettings }
+            this.ballSpeed = this.initialSettings.ballSpeed
+            this.scores = {...this.initialSettings.scores}
+            this.paddle1Settings = {...this.initialSettings.paddle1}
+            this.paddle2Settings = {...this.initialSettings.paddle2}
+            this.interval =this.initialSettings.interval
+            this.running = this.initialSettings.running
+            this.winner = this.initialSettings.winner
+
+            // use to loop through eachh paddle and also figure out how to it interacts with ball
             this.paddlesArr = [this.paddle1Settings, this.paddle2Settings]
             this.paddleOnApproach = { ...this.paddle2Settings }
-            this.interval =null
-            this.running = true
-            this.winner=null
-            
         }
-        clearGame=()=> {
+        clearGame = () => {
             const { width, height } = this.canvas
             this.ctx.clearRect(0, 0, width, height)
+        }
+        handleBallStartX() {
+            if (this.ballStart === 'player1') {
+                this.ballStart = 'player2'
+                return 10
+            } else {
+                this.ballStart = 'player1'
+               return this.width - this.paddleSettings.width
+            }
         }
         drawLine(length) {
             let inc = 0
@@ -99,11 +127,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 this.paddleOnApproach = {...this.paddle1Settings}
             }
         }
+
         drawPaddle=()=> {
             this.paddlesArr.forEach((obj) => {
                 createObject({ x:obj.x, y:obj.y, ...this.paddleSettings }, this.ctx)
             })
         }
+
         movePaddles=()=> {
             const runPaddles = (paddle, side, speed) => {
                 document.addEventListener('keydown', (e) => {
@@ -143,41 +173,44 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             })
         }
+
         runPaddles=()=> {
             this.drawPaddle()
             this.movePaddles()
         }
-        drawBall=()=> {
-            // createObject(this.ballSettings, this.ctx)
+
+        drawBall = () => {
             createBall(this.ballSettings, this.ctx)
         }
-        moveBall=()=> {
+
+        moveBall = () => {
+            
             const isPaddleSameHeight = this.ballSettings.y >= this.paddleOnApproach.y
-            && this.ballSettings.y <= this.paddleOnApproach.y + this.paddleSettings.height
+                && this.ballSettings.y <= this.paddleOnApproach.y + this.paddleSettings.height
+            const isPaddleSameWidth = this.ballSettings.x >= this.paddleOnApproach.x +10 && this.ballSettings.x <= this.paddleOnApproach.x
             const {x, xSpeed, ySpeed} = this.ballSettings
             this.ballSettings.x += xSpeed
             this.ballSettings.y += ySpeed
-
-            if (this.ballSettings.x === this.paddleOnApproach.x +10 && isPaddleSameHeight) {
-                this.ballSettings.xSpeed = this.ballSpeed 
-            } else if (this.ballSettings.x === this.paddleOnApproach.x - 10 && isPaddleSameHeight) {
-                this.ballSettings.xSpeed = -this.ballSpeed
-            }
 
             if (this.ballSettings.y <= 10) {
                 this.ballSettings.ySpeed = this.ballSpeed
             } else if (this.ballSettings.y >= this.height-10) {
                 this.ballSettings.ySpeed = -this.ballSpeed
             }
-
-    
+            if (this.ballSettings.x === this.paddleOnApproach.x +10 && isPaddleSameHeight) {
+                this.ballSettings.xSpeed = this.ballSpeed 
+            } else if (this.ballSettings.x === this.paddleOnApproach.x - 10 && isPaddleSameHeight) {
+                this.ballSettings.xSpeed = -this.ballSpeed
+            }
         }
+
         runBall() {
             this.drawBall()
             this.moveBall()
         }
+
         checkForScoreUpdate=()=> {
-            if (Object.values(this.scores).reduce((acc, s) => acc + s, 0) === 7) {
+            if (Object.values(this.scores).reduce((acc, s) => acc + s, 0) === 2) {
                 const winner = this.scores.padd1 > this.scores.padd2 ? 'Player 1' : 'Player 2'
                 this.winner = winner
                 this.running = false
@@ -198,19 +231,19 @@ document.addEventListener("DOMContentLoaded", () => {
             updateScores(padd1, padd2, this.round)
         }
         gameReset=()=> {
-            this.paddle1Settings = createPadd(0, this.height / 2 - this.paddleSettings.height, 0, { left: 's', right: 'w' })
-            this.paddle2Settings = createPadd(this.width - this.paddleSettings.width, this.height / 2 - this.paddleSettings.height, 0,
-                { left: 'ArrowLeft', right: 'ArrowRight' })
             this.ballSettings = {
-                x: 20,
-                y :this.height / 2 -30,
+                x: this.handleBallStartX(),
+                y :randomIntFromInterval(30,this.height-30),
                 radius:8,
-                xSpeed: 10,
-                ySpeed: 10,
+                xSpeed: 2.5,
+                ySpeed: 2.5,
             }
+            this.paddle1Settings = {...this.initialSettings.paddle1}
+            this.paddle2Settings = {...this.initialSettings.paddle2}
             this.paddlesArr =[this.paddle1Settings, this.paddle2Settings]
         }
         playGame = () => {
+            console.log(this.ballSettings.y)
             this.clearGame()
             this.updateGameScore()
             this.drawLine(100)
@@ -221,28 +254,36 @@ document.addEventListener("DOMContentLoaded", () => {
         winnerModal = () => {
             const body = document.querySelector('body')
             const modal = document.createElement('div')
+            const handleRestart = (e) => {
+                e.preventDefault()
+                location.reload()
+            }
             body.appendChild(modal)
             body.classList.add('background-change')
             modal.classList.add('modal')
             modal.innerHTML = `
             <h1>Game Over</h1>
             <p>The winner is ${this.winner}</p>
+            <button class='modal-btn'>Restart</button>
             `
+            document.querySelector('.modal-btn').addEventListener('click', handleRestart)
         }
         gameInterval = () => {
             if (this.running) {
-               this.playGame()
+                this.playGame()
+                this.interval = window.requestAnimationFrame(this.gameInterval)
             } else {
-                clearInterval(this.interval)
-                console.log('stopped')
+                // clearInterval(this.interval)
                 this.winnerModal()
+                window.cancelAnimationFrame(this.interval)
             }
-  
+
         }
         runGame = () => {
-         this.interval = setInterval(() => {
-                this.gameInterval()
-            }, 100);
+            window.requestAnimationFrame(this.gameInterval)
+        //  this.interval = setInterval(() => {
+        //         this.gameInterval()
+        //     }, 1000/13);
         }
         
     }
